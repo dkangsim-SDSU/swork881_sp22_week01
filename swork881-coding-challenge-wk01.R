6539a641aa9ea868f049aa97d947f5d5dada6fb3
dkangsim-SDSU <96759466+dkangsim-SDSU@users.noreply.github.com>
2022-01-20 15:43:31 -0800

    Add files via upload

---
+++ swork881-coding-challenge-wk01.R
@@ -0,0 +1,31 @@
+#### SWORK 881 GitHub exercise ####
+# NAME: 
+# REDID:
+
+#install tidyverse packages
+install.packages()
+
+#load tidyverse
+library()
+
+# read data in (the data file is LHS_exercise.csv inside the data folder)
+data01<- read_csv("")
+
+# inspect the data, you can use for example glimpse() to answer the following questions: 
+glimpse (data01)
+
+#How many variables are there in the data? (hint: check your environment window)
+
+#How many observations are there?
+
+
+
+# What Latino Subgroups are represented in the data? Indicate their frequency 
+ftable() #hint use latino_subgroup variable
+
+## provide your answer below ##
+
+
+
+# provide mean/sd or n/frequency for the following variables: age, income, parent_immigrant
+# and briefly describe your sample population based on the data
